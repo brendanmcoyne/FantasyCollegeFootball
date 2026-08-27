26dc1af25b825419cf77b983a3bd97f91aed87b6
brendanmcoyne <bmcoyne@bu.edu>
2026-08-27 14:32:18 -0400

    Used new deployed API link

--- src/api/cfbApi.ts
+++ src/api/cfbApi.ts
@@ -1,6 +1,6 @@
 import type { CollegeTeam } from '../types/football'
 
-const API_BASE_URL = 'http://localhost:8000'
+const API_BASE_URL = 'https://cfb-2025-api.vercel.app'
 
 export async function getTeams(): Promise<CollegeTeam[]> {
     const response = await fetch(`${API_BASE_URL}/teams`)
